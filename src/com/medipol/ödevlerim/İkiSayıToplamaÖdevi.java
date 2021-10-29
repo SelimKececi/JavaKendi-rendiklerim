b075ad8fd80ddb9345ace9831d71ed4d1179ff38
SelimKececi <burakselimkececi@hotmail.com>
2021-10-30 00:02:03 +0300

    Initial commit

---
+++ src/com/medipol/ödevlerim/İkiSayıToplamaÖdevi.java
@@ -0,0 +1,15 @@
+package com.medipol.ödevlerim;
+
+public class İkiSayıToplamaÖdevi {
+
+	public static void main(String[] args) {
+	int a = 2;
+	
+	int b = 4;
+	
+	System.out.println(a+b);
+		
+
+	}
+
+}
